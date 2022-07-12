5981237daa9c2b8e29598419af8517ec51a5c541
Ph.Zh <p.zhavoronkov@gmail.com>
2022-07-13 00:47:55 +0400

    updated logic in boolean method for leap year

--- src/Praktikum.java
+++ src/Praktikum.java
@@ -27,22 +27,20 @@ public class Praktikum {
         int year = scanner.nextInt();
 
         if (isLeapYear(year)) {
-            // здесь нужно вывести результат
-            if (isLeapYear(year)) {
-                System.out.println("12.09." + year);
-            } else {
-                System.out.println("13.09." + year);
-            }
+            // здесь нужно вывести результат{
+            System.out.println("12.09." + year);
+        } else {
+            System.out.println("13.09." + year);
         }
     }
 
     public static boolean isLeapYear(int year) {
         // здесь нужно определить, является ли переданный год високосным
-        if (year % 400 == 0) {
-            return true;
-        } else if (year % 100 != 0) {
+        if (year % 4 != 0) {
             return false;
-        } else if (year % 4 != 0){
+        } else if (year % 400 == 0) {
+            return false;
+        } else if (year % 100 == 0) {
             return false;
         } else {
             return true;
